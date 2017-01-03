a2c6291e73b819a810941ddbcf71e4cffc994d39
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2017-01-03 09:50:32 +0800

    完成基本的分页

---
+++ code/WebContent/js/member.js
@@ -0,0 +1,114 @@
+$(function() {
+	
+	
+	var dialog = $('.dialog-form').dialog({
+		width:530
+	});
+	
+	dialog.dialog('close');
+	
+	$('tbody .col-operations .btn-edit').on('click' , function() {
+		event.preventDefault();
+		var data = $(this).closest('tr').data('json');
+		
+		$('.dialog-form form input[name=id]').val(data.id);
+		$('.dialog-form form input[name=name]').val(data.name);
+		$('.dialog-form form input[name=nick-name]').val(data.nickName);
+		$('.dialog-form form input[name=tel-number]').val(data.telephoneNumber);
+		$('.dialog-form form input[name=m-number]').val(data.mobileNumber);
+		$('.dialog-form form input[name=w-price]').val(data.weightPrice);
+		$('.dialog-form form input[name=v-price]').val(data.volumnPrice);
+		$('.dialog-form form input[name=address]').val(data.address);
+		
+		dialog.dialog('open');
+	});
+	
+	$('tbody .col-operations .btn-delete').on('click' , function() {
+		event.preventDefault();
+		var data = $(this).closest('tr').data('json');
+		$('<div></div>').appendTo('body')
+		  .html('<div style="font-size: 18px;margin-top: 15;">确认删除这条记录？</div>')
+		  .dialog({
+		      modal: true, title: '提醒', zIndex: 10000, autoOpen: true,
+		      width: 'auto', resizable: false,
+		      buttons: {
+		          "确定": function () {
+		        	  $.ajax({
+		        		url : 'delete.html',  
+		        		type : 'POST',
+		        		dataType : 'json',
+		        		data : {'id' : data.id},
+		        		success : function(result) {
+		        			if (result.result) {
+		        				window.location.reload(true);
+		        			}
+		        		}
+		        	  });
+		          },
+		          "取消": function () {
+		              $(this).dialog("close");
+		          }
+		      },
+		      close: function (event, ui) {
+		          $(this).remove();
+		      }
+		});
+	});
+	
+	$('.btn-area .btns .btn-edit').on('click' , function() {
+	});
+	
+	$('.btn-area .btns .btn-save').on('click' , function(event) {
+		event.preventDefault();
+
+		var jsonArray = $('.dialog-form form').serializeArray();
+		var jsonData = {};
+		jsonArray.forEach(function(ele , index) {
+			jsonData[ele.name] = ele.value;
+		});
+		
+		$.ajax({ 
+			url : "save.html", 
+			data : jsonData,
+			dataType : 'json',
+			type : 'POST',
+			success: function(result){
+				if (result.result) {
+					window.location.reload(true);
+				}
+			}}
+		);
+	});
+	
+	$('.btn-area .btns .btn-cancel').on('click' , function() {
+		event.preventDefault();
+		dialog.dialog('close');
+	});
+	
+	$('.function-area .btn-create').on('click' , function() {
+		event.preventDefault();
+		
+		$('.dialog-form form input[name=id]').val('');
+		$('.dialog-form form input[name=name]').val('');
+		$('.dialog-form form input[name=nick-name]').val('');
+		$('.dialog-form form input[name=tel-number]').val('');
+		$('.dialog-form form input[name=m-number]').val('');
+		$('.dialog-form form input[name=w-price]').val('');
+		$('.dialog-form form input[name=v-price]').val('');
+		$('.dialog-form form input[name=address]').val('');
+		
+		$('.dialog-form').dialog({width:530});
+	});
+	
+	
+	var totalPage = $('.pagination').data('total-page');
+	var currentPage = $('.pagination').data('current-page');
+	$(".pagination").Page({
+      totalPages: totalPage,//分页总数
+      liNums: 7,
+      activeClass: 'activP', //active class style
+      callBack : function(page){
+    	  window.location.href='member.html?page=' + page;
+      }
+  });
+});
--- code/src/main/java/com/crunchify/controller/CrunchifyHelloWorld.java
+++ code/src/main/java/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -8,6 +8,7 @@
 package com.crunchify.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
@@ -18,6 +19,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
 import com.dao.TestDO;
+import com.wuliu.api.common.model.PageResultModel;
+import com.wuliu.api.member.model.WuliuMemberModel;
+import com.wuliu.api.member.model.WuliuMemberQueryParam;
 import com.wuliu.api.member.service.WuliuMemberService;
 import com.wuliu.api.order.model.WuliuOrderModel;
 import com.wuliu.api.order.model.WuliuOrderQueryParam;
@@ -57,46 +61,12 @@ public class CrunchifyHelloWorld {
 
         logger.info("=========");
         String message = "3234=====";
-        TestDO testDO = new TestDO();
-        testDO.setName("adsadsdf");
-        System.out.println("23");
-        System.out.println("1233");
+        WuliuMemberQueryParam wuliuMemberQueryParam = new WuliuMemberQueryParam();
+        PageResultModel<WuliuMemberModel> memberPageResult = wuliuMemberService.queryMembers(wuliuMemberQueryParam);
+        List<WuliuMemberModel>  members = memberPageResult.getResultList();
+        System.out.println(JSON.toJSONString(members));
 
-        /*
-         * WuliuOrderDetailModel wuliuOrderDetailModel = new WuliuOrderDetailModel();
-         * wuliuOrderDetailModel.setHeight(1L); wuliuOrderDetailModel.setLength(1L);
-         * wuliuOrderDetailModel.setMainOrderId(9L); wuliuOrderDetailModel.setStatus("enable");
-         * wuliuOrderDetailModel.setWeight(1L); wuliuOrderDetailModel.setWidth(1L); wuliuOrderDetailModel.setCount(1);
-         * wuliuOrderDetailService.addOrderDetail(wuliuOrderDetailModel); wuliuOrderDetailModel.setHeight(2L);
-         * wuliuOrderDetailModel.setLength(2L); wuliuOrderDetailModel.setMainOrderId(9L);
-         * wuliuOrderDetailModel.setStatus("disable"); wuliuOrderDetailModel.setWeight(2L);
-         * wuliuOrderDetailModel.setWidth(2L); wuliuOrderDetailModel.setCount(2);
-         * wuliuOrderDetailService.addOrderDetail(wuliuOrderDetailModel); wuliuOrderDetailModel.setHeight(3L);
-         * wuliuOrderDetailModel.setLength(3L); wuliuOrderDetailModel.setMainOrderId(10L);
-         * wuliuOrderDetailModel.setStatus("enable"); wuliuOrderDetailModel.setWeight(3L);
-         * wuliuOrderDetailModel.setWidth(3L); wuliuOrderDetailModel.setCount(3);
-         * wuliuOrderDetailService.addOrderDetail(wuliuOrderDetailModel); wuliuOrderDetailModel.setHeight(4L);
-         * wuliuOrderDetailModel.setLength(4L); wuliuOrderDetailModel.setMainOrderId(10L);
-         * wuliuOrderDetailModel.setStatus("enable"); wuliuOrderDetailModel.setWeight(4L);
-         * wuliuOrderDetailModel.setWidth(4L); wuliuOrderDetailModel.setCount(4);
-         * wuliuOrderDetailService.addOrderDetail(wuliuOrderDetailModel);
-         */
-
-        /*
-         * WuliuOrderQueryParam wuliuOrderQueryParam = new WuliuOrderQueryParam();
-         * wuliuOrderQueryParam.setCarIndex(123L);
-         * System.out.println(JSON.toJSONString(wuliuWholeOrderService.queryWholeOrders(wuliuOrderQueryParam)));
-         * wuliuOrderQueryParam.setCarIndex(124L);
-         * System.out.println(JSON.toJSONString(wuliuWholeOrderService.queryWholeOrders(wuliuOrderQueryParam)));
-         */
-
-        WuliuOrderQueryParam wuliuOrderQueryParam = new WuliuOrderQueryParam();
-        Date date = new Date(System.currentTimeMillis());
-        wuliuOrderQueryParam.setMinOrderDate(CalendarUtil.getMinDateInSameDay(date));
-        wuliuOrderQueryParam.setMaxOrderDate(CalendarUtil.getMaxDateInSameDay(date));
-        System.out.println(JSON.toJSONString(wuliuMergedOrderService.queryMergedOrders(wuliuOrderQueryParam)));
-
-        return new ModelAndView("index", "message", message);
+        return new ModelAndView("index", "members", members);
     }
 
     public SqlSessionTemplate getSqlSessionTemplate() {
