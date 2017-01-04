17a43376dd778ba3159dd7d38f56d802f96aaab2
yunbin.wangyb <yunbin.wangyb@alibaba-inc.com>
2017-01-04 13:45:40 +0800

    完成Order订单的基本展示

---
+++ code/src/main/java/com/wuliu/controller/Order.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2016 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package com.wuliu.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSON;
+import com.wuliu.api.common.model.PageResultModel;
+import com.wuliu.api.member.constant.WuliuMemberConst;
+import com.wuliu.api.member.model.WuliuMemberModel;
+import com.wuliu.api.member.model.WuliuMemberQueryParam;
+import com.wuliu.api.member.service.WuliuMemberService;
+import com.wuliu.api.order.model.WuliuOrderModel;
+import com.wuliu.api.order.model.WuliuOrderQueryParam;
+import com.wuliu.api.order.service.WuliuOrderService;
+import com.wuliu.api.orderbusiness.model.WuliuWholeOrderModel;
+import com.wuliu.api.orderbusiness.service.WuliuMergedOrderService;
+import com.wuliu.api.orderbusiness.service.WuliuWholeOrderService;
+import com.wuliu.api.orderdetail.service.WuliuOrderDetailService;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+/**
+ * 类CrunchifyHelloWorld.java的实现描述：TODO 类实现描述
+ * 
+ * @author yunbin.wangyb 2016年12月27日 下午4:36:05
+ */
+
+@SuppressWarnings("all")
+@Controller
+public class Order {
+
+    private final static Logger     logger = LoggerFactory.getLogger(Order.class);
+
+    private WuliuMemberService      wuliuMemberService;
+
+    private WuliuOrderService       wuliuOrderService;
+
+    private WuliuOrderDetailService wuliuOrderDetailService;
+
+    private WuliuWholeOrderService  wuliuWholeOrderService;
+
+    private WuliuMergedOrderService wuliuMergedOrderService;
+
+    @RequestMapping("/order")
+    public ModelAndView load(@RequestParam(value = "page", required = false) Integer page) throws UnsupportedEncodingException {
+
+        WuliuOrderQueryParam wuliuOrderQueryParam = new WuliuOrderQueryParam();
+
+        PageResultModel<WuliuWholeOrderModel> result = wuliuWholeOrderService.queryWholeOrders(wuliuOrderQueryParam);
+        Map<String, Object> returnMap = new HashMap<String, Object>();
+        result.getResultList().addAll(result.getResultList());
+        returnMap.put("orders", result);
+        return new ModelAndView("order", returnMap);
+    }
+
+    public WuliuMemberService getWuliuMemberService() {
+        return wuliuMemberService;
+    }
+
+    public void setWuliuMemberService(WuliuMemberService wuliuMemberService) {
+        this.wuliuMemberService = wuliuMemberService;
+    }
+
+    public WuliuOrderService getWuliuOrderService() {
+        return wuliuOrderService;
+    }
+
+    public void setWuliuOrderService(WuliuOrderService wuliuOrderService) {
+        this.wuliuOrderService = wuliuOrderService;
+    }
+
+    public WuliuOrderDetailService getWuliuOrderDetailService() {
+        return wuliuOrderDetailService;
+    }
+
+    public void setWuliuOrderDetailService(WuliuOrderDetailService wuliuOrderDetailService) {
+        this.wuliuOrderDetailService = wuliuOrderDetailService;
+    }
+
+    public WuliuWholeOrderService getWuliuWholeOrderService() {
+        return wuliuWholeOrderService;
+    }
+
+    public void setWuliuWholeOrderService(WuliuWholeOrderService wuliuWholeOrderService) {
+        this.wuliuWholeOrderService = wuliuWholeOrderService;
+    }
+
+    public WuliuMergedOrderService getWuliuMergedOrderService() {
+        return wuliuMergedOrderService;
+    }
+
+    public void setWuliuMergedOrderService(WuliuMergedOrderService wuliuMergedOrderService) {
+        this.wuliuMergedOrderService = wuliuMergedOrderService;
+    }
+}
